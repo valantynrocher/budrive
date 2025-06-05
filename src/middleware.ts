d7d9a6485c16c90de51eb4ad4ae738479c643881
valantynrocher <valentinrocher@gmail.com>
2025-06-06 00:19:33 +0200

    remove middleware (need to recreate it later)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
+export const middleware = async (request: NextRequest) =>
+  NextResponse.next({
+    request,
+  });
 
 export const config = {
   matcher: [
